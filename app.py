225a4ff49c771e6a12d1d96d29657cb0af3513e2
edencse <eia2614@mavs.uta.edu>
2025-04-16 20:50:03 -0500

    Fix port binding for deployment

--- app.py
+++ app.py
@@ -428,5 +428,5 @@ if __name__ == '__main__':
     conn.commit()
     conn.close()
     print("✅ Tables created successfully!")  # Confirm that tables are created
-     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
